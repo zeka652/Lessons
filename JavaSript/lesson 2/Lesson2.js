e80b624b921fe66cb7b5f8a401a60a53e8f160a5
Wonty <samoulovev@gmail.com>
2019-02-16 11:52:13 +0200

    first commit

---
+++ JavaSript/lesson 2/Lesson2.js
@@ -0,0 +1,78 @@
+
+
+var i = 0;
+do{
+	console.log(i)
+	i++;
+} while(<=10)*/
+
+
+
+
+/*var N
+var A = 8
+var B = 18
+for (var i = 0; i<=B  ; i++) 
+
+
+for (var i = 0; i<=10 ; i++) {
+ 	console.LOG(i);
+ }
+var i = 0;
+while(i<=20){
+	console.log(i);
+	 i++;
+}
+
+
+
+ var nuber = 15
+ var string = "";
+
+if (n) <= 0 || number >= N{
+	console.log(`${nuber} its not numder`);
+}
+else{
+ 	if (nuber % 2 == 0) {
+ 	string = "chetk"
+	}
+	else{
+	string = "nechetk"
+	}
+	if (nuber >=10 && number <99) {
+	string = `${string} twotwist`;
+
+	}
+	else(number >=100 && number <=999) {
+	string = `${string} 3twist`;
+	}
+	console.log(`${string} nuber`)
+
+
+
+var string1 = "1", string2 = "мыла", string3 = "Раму";
+var count = 10;
+var string4 = "раз";
+var space =
+
+console.log(string1) + string2 + string3 + count + space + string4;
+console.log(`${string1} ${string2} ${string3} ${count} ${string4}`);
+else (
+	console.log("Четное")
+
+
+var year = 2020;
+ype = "";
+if (year % 4 == 0) {
+	type = 366;
+	if(year % == 0 && year % 400 != 0){
+		type = 365
+	}	
+}
+else{
+	year = 365;
+}
+if (year == 365) {
+
+}
+*/
---
+++ MyWorks/script.js
@@ -0,0 +1,25 @@
+/*ost = (10 % 3);
+console.log(ost);
+var numb = 1;
+var isboo = true;
+var numb2 = 2;
+if (numb == isboo) {
+	alert("true");
+}
+else{
+	alert("false");
+}
+
+/*var test = "Hello", age = 15, name="Wonty";
+
+test = "Do miss me ?";
+
+console.log(test
+//console.log(7*5)
+//console.log("hello Wonty");
+//console.info("hello Wonty!")
+/*alert("hello world!");
+*/alert("hello,I'm Mr.W[0]nty™ the creator of this site!");/*
+alert("hello world3!");
+alert("hello world4!");
+alert("hello world5!");*/
